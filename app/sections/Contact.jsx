3b65e3ee6deb4f37fa0f016c440e6da3710ea4bf
jakariya0dev <jhalhadi@gmail.com>
2025-08-11 22:30:45 +0600

    experience updated

--- app/sections/Contact.jsx
+++ app/sections/Contact.jsx
@@ -83,7 +83,7 @@ export default function Contact() {
           </div>
           <div>
             <p className="font-semibold">📧 Email:</p>
-            <p className="text-gray-400">{info.email} or jhalhadi@gmail.com</p>
+            <p className="text-gray-400">{info.email}</p>
           </div>
           <div>
             <p className="font-semibold">📞 WhatsApp:</p>
